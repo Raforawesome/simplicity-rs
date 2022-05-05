053d80aac25b792dcaa046dd7cf852deec0643b2
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-05-05 10:25:38 -0400

    Change get_targets to take String and not Vec<String>

--- src/handler/commands/utils.rs
+++ src/handler/commands/utils.rs
@@ -1,9 +1,10 @@
 use super::prelude::*;
 use serenity::model::user::User;
 
-fn get_targets<'a>(mentions: &'a [User], args: &[String]) -> Option<&'a User> {
+fn get_targets<'a>(mentions: &'a [User], arg: &str) -> Option<&'a User> {
 	if !mentions.is_empty() {
 		return Some(&mentions[0]);
 	}
+	let r: Option<&'a User> = None;
 	todo!()
 }
